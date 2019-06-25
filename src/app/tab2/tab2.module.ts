a59956363a9f134234afd5d49792ce40bd446dab
stephan <webmaster1@beyondjava.de>
2019-06-25 22:29:29 +0200

    demo for issue stephanrauh/ngx-extended-pdf-viewer/#86

---
+++ src/app/tab2/tab2.module.ts
@@ -0,0 +1,13 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab2Page } from './tab2.page';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
+@NgModule({
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule.forChild([{ path: '', component: Tab2Page }]), NgxExtendedPdfViewerModule],
+  declarations: [Tab2Page]
+})
+export class Tab2PageModule {}
